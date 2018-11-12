322684504b8a6f7142e1c01beb2e665a9c3edcf3
Karen Casey-Gover <karen.caseygover@gmail.com>
2018-11-12 11:51:00 -0400

    Add store with fake data & addtodo method

--- app/src/store.js
+++ app/src/store.js
@@ -4,13 +4,29 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  strict: true,
   state: {
-
+    todos: [{ id: 1, title: 'srtjdydkyfk', completed: false }],
+    newId: 2,
+    editedTodo: null,
+    visibility: 'all',
   },
   mutations: {
+    addTodo(state, value) {
+      if (!value) {
+        return;
+      }
+      state.todos.push({
+        id: state.newId++,
+        title: value,
+        completed: false,
+      });
+    },
 
   },
   actions: {
-
+    addTodo(context, text) {
+      context.commit('addTodo', text);
+    }
   },
 });
